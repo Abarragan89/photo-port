ac051f3c9d7aaa0532d402bb17fb336205f40678
Anthony Barragan <anthony.bar.89@gmail.com>
2022-04-30 18:37:28 -0700

    added tests for Nav and About components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import About from './components/About'
-import Nav from './components/Nav'
+import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
   return (
