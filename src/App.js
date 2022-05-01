dabab6e6b5f1222a7f76add7c55872598c0aa421
Anthony Barragan <anthony.bar.89@gmail.com>
2022-05-01 10:02:20 -0700

    made hooks to change state to Nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+      {
+          name: "commercial",
+          description:"photos of grocery stores, food trucks, and other commercial products",
+      },
+      {
+          name: "portraits",
+          description: "Portraits of people in my life" 
+      },
+      {
+          name: "food", 
+          description: "Delicious delcacies"
+      },
+      {
+          name: "landscape",
+          description: "Fields, farmhouses, waterfalls, and the beauty of nature"
+      }
+  ])
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <>
+    <Nav 
+    categories={categories}
+    setCurrentCategory={setCurrentCategory}
+    currentCategory={currentCategory}
+    />
     <main>
-      <Nav />
+      <Gallery />
       <About />
     </main>
     </>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,32 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav() {
-    const categories = [
-        {
-            name: "commercial",
-            description:"photos of grocery stores, food trucks, and other commercial products",
-        },
-        {
-            name: "portraits",
-            description: "Portraits of people in my life" 
-        },
-        {
-            name: "food", 
-            description: "Delicious delcacies"
-        },
-        {
-            name: "landscape",
-            description: "Fields, farmhouses, waterfalls, and the beauty of nature"
-        }
-    ];
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-    }
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props;
+    // useEffect, first parameter is a callback function. Second parameter is the what will trigger a rerender if changed (ie currentCategory)
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory])
     return (
-        <header>
+        <header className="flex-row px-1">
             <h2>
                 <a href="/" data-testid="link">
-                    <span role="img" aria-label="camera">📸</span> Oh Snap!
+                    <span role="img" aria-label="camera">
+                        {" "}
+                        📸 
+                        </span> {" "}
+                        Oh Snap!
                 </a>
             </h2>
             <nav>
@@ -40,9 +33,10 @@ function Nav() {
                         <span>Contact</span>
                     </li>
                     {categories.map((category) => (
-                        <li className="mx-1" key={category.name}>
-                            <span onClick={() => categorySelected(category.name)}>
-                                {category.name}
+                        // if currentCategory name is category.name, then the 'navActive' will be evaluated. 
+                        <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
+                            <span onClick={() => setCurrentCategory(category)}>
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
